550b5abf89d3852e1a36a53dda3b4d709c3bbcc8
beomsu <beumsu1221@gmail.com>
2023-06-29 17:25:12 +0900

    0629 React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Counter from "./components/Counter";
-import Counter2 from "./components/Counter2";
+import TodoInput from "./components/todo/TodoInput";
+import ZCounter from "./components/zcounter";
+import SampleLayout from "./layout/SampleLayout";
+import TemplatePage from "./layout/TemplatePage";
 
 
 function App() {
   return (
-  <div className="flex m-12">
-    <Counter2></Counter2>
+  <div>
+    <TodoInput></TodoInput>
     </div>
   );
 }
